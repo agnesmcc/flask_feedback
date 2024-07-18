4823cdf91cfdc49a73a9174ea8af5e27f3c102fb
Agnes <agnes.mccall5@gmail.com>
2024-07-17 20:59:05 -0400

    Create Feedback model

--- models.py
+++ models.py
@@ -46,6 +46,15 @@ class User(db.Model):
             return False
 
 
+class Feedback(db.Model):
+    """Feedback."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    username = db.Column(db.String(20), db.ForeignKey('user.username'))
+
+
 # DO NOT MODIFY THIS FUNCTION
 def connect_db(app):
     """Connect to database."""
